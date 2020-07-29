8e4c9b3a865a70bd6defddbe7eb1ff3de740ca10
Jake Haberle <jake.haberle@gmail.com>
2020-07-28 22:17:51 -0600

    updates to JS, sidebar still not working but is appending

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var citySearch = "";
+// Initial Cities in sidebar
+
+var cities = ["Denver", "Atlanta", "Phoenix"];
+
+function newButton (){
+    $(".side-button").empty();
+    for (var i = 0; i < cities.length; i++){
+        var newCity = $("<button>");
+        newCity.addClass("button");
+        newCity.attr("data-city", cities[i]);
+        newCity.text(cities[i]);
+        $("#side-button").append(newCity);
+    }
+}
 
 
-$("#city-search").on("click", function(){
-   
-    alert("I just searched for city  " + citySearch + "!");
-
-    var citySearch = $("#city-input");
-    
-
-
+$("#search-button").on("click", function(event){
+        event.preventDefault();
+        var citySearch = $("#city-input").val().trim();
+        cities.push(citySearch);
+        newButton();
 });
+
+
 
 
 
